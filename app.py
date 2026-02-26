b1c89d0c04d1e1176587f6d1d6ffebef2ef07185
integraleacademy <clement.annecy@gmail.com>
2026-02-26 15:33:33 +0100

    Fix hardcoded Flask debug mode

--- app.py
+++ app.py
@@ -13554,4 +13554,5 @@ def admin_sessions_slash_redirect():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    debug_enabled = os.environ.get("FLASK_DEBUG", "0") == "1"
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=debug_enabled)
